c1bd205602855b623ff2c7ac8d2e44d86e974b69
PhamDy <phamdacdy@gmail.com>
2024-03-29 23:10:00 +0700

    thread

--- threadUseser/src/main/java/com/example/demo/service/UsersService.java
+++ threadUseser/src/main/java/com/example/demo/service/UsersService.java
@@ -11,8 +11,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.*;
 
