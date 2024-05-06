8802bdca796d21144216df482b3533307208722d
PhamDy <phamdacdy@gmail.com>
2024-05-06 17:48:52 +0700

    Kafka

---
+++ Microservices/Kafka/kafka-tutorial/src/main/java/com/DyPham/kafkatutorial/kafka/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.DyPham.kafkatutorial.kafka;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "javaguides", groupId = "myGroup")
+    public void consumer(String message){
+        LOGGER.info(String.format("Message received -> %s", message));
+    }
+
+}
